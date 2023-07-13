bbf33d79c91a11e97605f580beb845f4e196e92a
Oghenetega Etareh <tega.etare@gmail.com>
2023-07-13 11:12:41 -0400

    Setting up the table

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-
+import React from 'react'
 
 import './App.css'
+import ContactList from './components/ContactList'
 
 function App() {
 
   return (
     <>
-      
+      <ContactList />
     </>
   )
 }
