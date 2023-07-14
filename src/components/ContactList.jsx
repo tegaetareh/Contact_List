ea7d2a1544f3bdbdc13508b11b3e28103805b20a
Oghenetega Etareh <tega.etare@gmail.com>
2023-07-13 22:25:16 -0400

    Fetching data and useEffect

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,5 +1,5 @@
 import React from "react"; 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ContactRow from "./ContactRow";
 
 const dummyContacts = [
@@ -10,6 +10,20 @@ const dummyContacts = [
 
 export default function ContactList() { 
 const [contacts, setContacts] = useState(dummyContacts)
+useEffect(() => {
+    async function fetchContacts() {
+      try {
+        const response = await fetch(
+          "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+        );
+        const result = await response.json();
+        setContacts(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchContacts();
+  }, []);
 console.log("Contacts: ", contacts);
   return ( 
         <table>
